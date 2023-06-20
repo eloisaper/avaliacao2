96d78ded3f52893e120090221a98974d63113a2c
eloisaper <eloisaperuzzo2000@outlook.com>
2023-06-20 08:21:08 -0300

    parte1 20/06

--- conexao.php
+++ conexao.php
@@ -2,7 +2,7 @@
     $hostname = "localhost";
     $username = "root";
     $password = "";
-    $database = "projeto1";
+    $database = "prova2";
     $port = 3307;
     $con = mysqli_connect($hostname, $username, $password, $database, $port);
     if (mysqli_connect_errno()) {
