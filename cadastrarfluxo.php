44f03f096e3113baf1bd29a4e12257aa75b18e73
eloisaper <eloisaperuzzo2000@outlook.com>
2023-06-20 08:14:46 -0300

    parte1 20/06

---
+++ cadastrarfluxo.php
@@ -0,0 +1,27 @@
+<?php
+    include("conexao.php");
+
+    $data = $_POST['data'];
+    $tipo = $_POST['tipo'];
+    $valor = $_POST['valor'];
+    $historico = $_POST['historico'];
+    $cheque = $_POST['cheque'];
+   
+
+    echo "<h1>Dados do Fluxo</h1>";
+    echo "Data: $data <br>";
+    echo "Tipo: $tipo <br>";
+    echo "Valor: $valor <br>";
+    echo "Histórico: $historico <br>";
+    echo "Cheque: $cheque <br>";
+
+    $sql = "INSERT INTO fluxo_caixa (data, tipo, valor, historico, cheque)";
+
+    $sql .= "VALUES ('".$data."','".$tipo."','".$valor."','".$historico."','".$cheque."')";
+    echo $sql."<br>";
+    $result = mysqli_query($con, $sql);
+    if($result)
+        echo "Dados cadastrados com sucesso!";
+    else    
+        echo "Erro ao tentar cadastrar!";
+?>
